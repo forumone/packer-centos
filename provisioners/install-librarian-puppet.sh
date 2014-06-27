c1f6f41904be93dd7f5508e41a797146cb71a354
William Hurley <whurley@forumone.com>
2014-06-27 14:18:12 -0400

    Updating to install Ruby 2.0.0-p481

--- provisioners/install-librarian-puppet.sh
+++
@@ -1,3 +0,0 @@
-yum -q -y makecache
-yum -q -y install git
-gem install librarian-puppet -v 1.0.3 --no-ri --no-rdoc
--- provisioners/upgrade-ruby.sh
+++ provisioners/upgrade-ruby.sh
@@ -1,7 +1,16 @@
-mkdir /tmp/ruby && cd /tmp/ruby
-curl ftp://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p481.tar.gz | tar xz
-cd ruby-2.0.0-p481
-./configure --disable-install-rdoc
-make && make install
-cd / && rm -fr /tmp/ruby
-gem update --system
+yum -q -y makecache
+yum -y install git openssl-devel
+
+git clone git://github.com/sstephenson/rbenv.git /usr/local/rbenv
+mkdir -p /usr/local/rbenv/plugins
+git clone git://github.com/sstephenson/ruby-build.git /usr/local/rbenv/plugins/ruby-build
+echo 'export RBENV_ROOT=/usr/local/rbenv' >> /etc/profile.d/ruby.sh
+echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> /etc/profile.d/ruby.sh
+echo 'eval "$(rbenv init -)"' >> /etc/profile.d/ruby.sh
+echo 'rbenv install 2.0.0-p481'
+source /etc/profile.d/ruby.sh; rbenv install 2.0.0-p481 -v
+echo 'rbenv global 2.0.0-p481'
+source /etc/profile.d/ruby.sh; rbenv global 2.0.0-p481
+echo 'rbenv rehash'
+source /etc/profile.d/ruby.sh; rbenv rehash
+source /etc/profile.d/ruby.sh; gem install librarian-puppet --no-ri --no-rdoc
